06e5bffae6e9ae21c12d18bdfd507a7cbe7822e5
rja907 <kmrrajj@gmail.com>
2018-01-01 00:11:22 +0530

    palindrome solved.

--- palindrome/index.js
+++ palindrome/index.js
@@ -25,8 +25,14 @@
 //   return str === rev;
 // }
 
+// function palindrome(str) {
+//   return str === str.split('').reduce((rev, i) => i + rev, '');
+// }
+
 function palindrome(str) {
-  return str === str.split('').reduce((rev, i) => i + rev, '');
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
