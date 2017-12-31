f6d277e4e475dc30dc61b60745ce62216f805e1e
rja907 <kmrrajj@gmail.com>
2017-12-31 23:45:43 +0530

    reversestring solved.

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   let rev = '';
+//   for (let s of str) {
+//     rev = s + rev;
+//   }
+//   return rev;
+// }
+
+function reverse(str) {
+  return str.split('').reduce((rev, i) => i + rev, '');
+}
 
 module.exports = reverse;
