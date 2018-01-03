ec4490c88bcd126cf4b50268c6b79759bee845ac
rja907 <kmrrajj@gmail.com>
2018-01-03 23:54:49 +0530

    pyramid solved.

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,37 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   const mid = Math.floor((2 * n - 1) / 2);
+//   for (var i = 0; i < n; i++) {
+//     let stair = '';
+//     for (var j = 0; j < 2 * n - 1; j++) {
+//       if (mid - i <= j && mid + i >= j) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+function pyramid(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+  if (stair.length === 2 * n - 1) {
+    console.log(stair);
+    return pyramid(n, row + 1);
+  }
+  const mid = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (mid - row <= stair.length && mid + row >= stair.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, stair + add);
+}
 
 module.exports = pyramid;
