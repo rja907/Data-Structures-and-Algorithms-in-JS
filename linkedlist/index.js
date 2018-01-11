c98c7f8006c88ead7b243f43aff8bf34e0b047be
rja907 <kmrrajj@gmail.com>
2018-01-11 23:48:38 +0530

    LL constructor, insertFirst and size

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,6 +2,7 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
+//Node constructor
 class Node {
   constructor(data, next = null) {
     this.data = data;
@@ -9,7 +10,25 @@ class Node {
   }
 }
 
-class LinkedList {}
+//LinkedList constructor
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  //insertFirst
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+  //size
+  size(list) {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+}
 
-module.exports = { Node, LinkedList };
 module.exports = { Node, LinkedList };
