fea280ed99398840d9de886503940661c5efb345
rja907 <kmrrajj@gmail.com>
2018-01-27 10:07:00 +0530

    forOf solved

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -141,6 +141,13 @@ class LinkedList {
       counter++;
     }
   }
+  *[Symbol.iterator](){
+    let node = this.head;
+    while(node){
+      yield node;
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
