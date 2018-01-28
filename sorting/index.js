d93f5f64fb93c1517fb786e95c0a5c73258eb794
rja907 <kmrrajj@gmail.com>
2018-01-28 23:08:15 +0530

    selectionsort solved

--- sorting/index.js
+++ sorting/index.js
@@ -15,7 +15,21 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+  //prove me wrong algorithm.
+  for(let i = 0; i < arr.length; i++){
+    let minIndex = i;
+    for(let j = i+1; j < arr.length; j++){
+      if(arr[j] < arr[minIndex]){
+        minIndex = j;
+      }
+    }
+    if(minIndex !== i){
+      let lesser = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
